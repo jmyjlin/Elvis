b3c3bdfe1bb498e439c113c203e2a793b37495d0
Jimmy Lin <jmyjlin@gmail.com>
2025-10-04 14:24:25 -0700

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Jimmy Lin on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
